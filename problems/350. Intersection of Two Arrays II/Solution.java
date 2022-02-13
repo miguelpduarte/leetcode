0b13f0fb5b26cb107ba9936052c7c5972921b770
miguelpduarte <miguelpduarte98@gmail.com>
2022-02-13 13:21:57 +0000

    Fix Java "unit tests"

--- problems/350. Intersection of Two Arrays II/Solution.java
+++ problems/350. Intersection of Two Arrays II/Solution.java
@@ -23,10 +23,10 @@ class Solution {
         Solution s = new Solution();
         // Input: nums1 = [1,2,2,1], nums2 = [2,2]
         // Output: [2,2]
-        assert Arrays.equals(s.intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2}), new int[]{2, 2});
+        System.out.println(Arrays.equals(s.intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2}), new int[]{2, 2}));
         // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
         // Output: [4,9]
         // Explanation: [9,4] is also accepted.
-        assert Arrays.equals(s.intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}), new int[]{4, 9});
+        System.out.println(Arrays.equals(s.intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}), new int[]{4, 9}));
     }
 }
