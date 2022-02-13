050b73fc16dc0b12906840d6cf69e9f61e6c50e5
miguelpduarte <miguelpduarte98@gmail.com>
2022-02-13 13:25:12 +0000

    Improve problem 349 solution
    Only one set is necessary, same approach as 350

--- problems/349. Intersection of Two Arrays/Solution.java
+++ problems/349. Intersection of Two Arrays/Solution.java
@@ -8,14 +8,15 @@ class Solution {
         for (int num: nums1) {
             unique1.add(num);
         }
-        HashSet<Integer> unique2 = new HashSet<>();
+        ArrayList<Integer> result = new ArrayList<>();
         for (int num: nums2) {
-            unique2.add(num);
+            if (unique1.contains(num)) {
+                result.add(num);
+                unique1.remove(num);
+            }
         }
 
-        unique1.retainAll(unique2);
-
-        return unique1.stream().mapToInt(Integer::intValue).toArray();
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
     public static void main(String[] args) {
         Solution s = new Solution();
@@ -25,6 +26,6 @@ class Solution {
         // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
         // Output: [9,4]
         // Explanation: [4,9] is also accepted.
-        System.out.println(Arrays.equals(s.intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}), new int[]{4, 9}));
+        System.out.println(Arrays.equals(s.intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}), new int[]{9,4}));
     }
 }
