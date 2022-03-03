cd3ac5ac1e71833570dd1ec8d971b672b86198fc
miguelpduarte <miguelpduarte98@gmail.com>
2022-03-03 12:49:49 +0000

    Significantly improve problem 121 solution

--- problems/121. Best Time to Buy and Sell Stock/Solution.java
+++ problems/121. Best Time to Buy and Sell Stock/Solution.java
@@ -1,26 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int max_profit = 0;
+        int max_profit_so_far = 0;
+        int bought_at = prices[0];
 
-        for (int i = 0; i < prices.length; ++i) {
-            int curr_max_profit = maxProfitBuyAt(i, prices);
-            max_profit = Math.max(max_profit, curr_max_profit);
+        for (int i = 1; i < prices.length; ++i) {
+            // Found new lowest price. Update the bought at value and skip iteration
+            if (prices[i] < bought_at) {
+                bought_at = prices[i];
+                continue;
+            }
+
+            int curr_profit = prices[i] - bought_at;
+            if (curr_profit > max_profit_so_far) {
+                max_profit_so_far = curr_profit;
+            }
         }
 
-        return max_profit;
-    }
-
-    private int maxProfitBuyAt(int day_bought, int[] prices) {
-        int max_profit = 0;
-        int bought_at = prices[day_bought];
-
-        for (int i = day_bought + 1; i < prices.length; ++i) {
-            int sold_at = prices[i];
-            int profit = sold_at - bought_at;
-
-            max_profit = Math.max(max_profit, Math.max(profit, 0));
-        }
-
-        return max_profit;
+        return max_profit_so_far;
     }
 }
